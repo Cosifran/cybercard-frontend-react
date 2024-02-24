3ca98598f3d71e9bf7cd09114a86ced80076a239
Francisco Backend <90176645+Cosifran@users.noreply.github.com>
2024-02-24 15:24:38 -0500

    se crea el inicio del proyecto landing-scourt

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function NotFound(second) {
+    return(
+        <html>
+            <body>
+                <h1>
+                    Esta pagina no existe
+                </h1>
+            </body>
+        </html>
+    )
+}
